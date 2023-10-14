586968f395f4707bc9bac29d1f3121abc20dc5cb
Shreea Sri <shreeasri@gmail.com>
2023-10-14 22:07:14 +0530

    config file, helper file and error handling

--- src/js/model.js
+++ src/js/model.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config.js';
+import { getJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -7,12 +9,7 @@ export const state = {
 //change only the state object but not returning anything
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     let { recipe } = data.data;
     state.recipe = {
@@ -27,6 +24,6 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 😖😖😖`);
   }
 };
