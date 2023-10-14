373d3a52a630cbac1f02d091f65984148b955c41
Shreea Sri <shreeasri@gmail.com>
2023-10-14 15:30:47 +0530

    implemented haschchange and load event

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,10 +30,17 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    //taking the hash dynamically from url
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    //if id is not their then simply return
+    if (!id) return;
+
     //1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8fd'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -160,4 +167,6 @@ const showRecipe = async function () {
 fetch(
   'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
 );
-showRecipe();
+
+//adding hashchange and load event
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
